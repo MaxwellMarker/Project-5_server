70f7f10caf29e57864c894e9c8d76fdc5ead28ef
Max Marker <maxmarker98@gmail.com>
2020-09-27 14:12:42 -0400

    basic setup with mongo, switching to dev after this

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Database
+const MONGO_URI = process.env.MONGO_URI;
+const db = mongoose.connection;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+db.on("open", () => {
+    console.log('connected to mongo')
+})
+
+// Middleware
+
+
+
+// Controllers
+
+
+
+// Listener
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
