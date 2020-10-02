fb1af7b8665f5f15725a5015e800e67a3fbe07f2
Max Marker <maxmarker98@gmail.com>
2020-10-01 23:24:50 -0400

    review controller routes

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { get } = require('mongoose');
-const router = express.router();
+const router = express.Router();
 const Review = require('../models/review');
 
-router.get('/all/:gameid', (req, res) => {
-    Review.find({gameId: req.params.gameId}, (error, reviews) => {
+router.get('/', (req, res) => {
+    Review.find({}, (error, reviews) => {
         if(error){
             res.status(500).json(error)
         }else{
@@ -13,5 +13,24 @@ router.get('/all/:gameid', (req, res) => {
     })
 })
 
+router.get('/:gameId', (req, res) => {
+    Review.find({gameId: `${req.params.gameId}`}, (error, reviews) => {
+        if(error){
+            res.status(500).json(error)
+        }else{
+            res.status(200).json(reviews);
+        }
+    })
+})
+
+router.post('/', (req, res) => {
+    Review.create(req.body, (error, review) => {
+        if(error){
+            res.status(500).json(error)
+        }else{
+            res.status(200).json(review);
+        }
+    })
+})
 
 module.exports = router;
